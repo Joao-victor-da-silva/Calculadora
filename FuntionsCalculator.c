1f6a7303bb137a8a40d3499673be9f3e25ed0831
Ana Julia Mantovani <anajuliamantovani001@gmail.com>
2024-10-16 23:11:19 -0300

    Add multiplication funtions and fixed some troubles
    add multiplication's funntion and fixed some error of sintaxe

--- FuntionsCalculator.c
+++ FuntionsCalculator.c
@@ -7,9 +7,9 @@ int sum (int num1, int num2)
     return num1 + num2;
 }
 
-int subtraction(int num1, int)
+int subtraction(int num1, int num2)
 {
-    return 0;
+    return num1 - num2;
 }
 
 int division(int num1, int num2)
@@ -17,7 +17,7 @@ int division(int num1, int num2)
     return num1 / num2;
 }
 
-int multiplication()
+int multiplication(int num1, int num2)
 {
-    return 0;
+    return num1 * num2;
 }
--- main.c
+++ main.c
@@ -11,28 +11,31 @@ int main()
     printf("=====================");
 
     printf("\n 1.Soma\n 2.Subtracao\n 3.Divisao\n 4.Multiplicacao\n 5.Sair\n");
+    printf("O que deseja fazer ? Digite o numero correspondente:");
     scanf("%d", &chose);
+
 
     switch (chose)
     {
         case 1:
-            printf("%d", sum(55, 60));
+            printf("\n%d\n\n", sum(55, 60));
             break;
 
         case 2:
-            printf("%d", subtraction(10, 5));
+            printf("\n%d\n", subtraction(10, 5));
             break;
 
         case 3:
-            printf("%d", division(10, 5));
+            printf("\n%d\n", division(10, 5));
             break;
 
         case 4:
-            division();
+            printf("\n%d\n", multiplication(10, 5));
             break;
 
         case 5:
             return 0;
     }
+    main();
     return 0;
 }
